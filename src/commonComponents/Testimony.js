52299aa39c9ecc9bd223851fe8ad5f68a44ed7f1
tanmayrudra <tanmayrudra555@gmail.com>
2022-12-07 02:02:50 +0530

    testi video

--- src/commonComponents/Testimony.js
+++ src/commonComponents/Testimony.js
@@ -6,6 +6,11 @@ import { Container, Row, Col } from "react-bootstrap";
 // Css
 import "./commonStyle/Testimony.scss";
 
+// Images import  
+import play_point from "../assets/ppc-play.svg";
+import video_pic from "../assets/video-pic.jpg";
+
+
   function Testimony(props) {
     return (
       <div className="reviews_panle">
@@ -16,8 +21,30 @@ import "./commonStyle/Testimony.scss";
                 <h2>See What Clients Are Saying</h2>
               </div>
             </Col>
-            <Col lg={6}></Col>
-            <Col lg={6}></Col>
+            <Col lg={4}>
+              <div className="banner_video">
+                <img src={video_pic} className="img-fluid plays" alt={video_pic}/>
+                <a href="/">
+                  <img src={play_point} width="70" alt={play_point}/>
+                </a>
+              </div>
+            </Col>
+            <Col lg={4}>
+              <div className="banner_video">
+                <img src={video_pic} className="img-fluid plays" alt={video_pic}/>
+                <a href="/">
+                  <img src={play_point} width="70" alt={play_point}/>
+                </a>
+              </div>
+            </Col>
+            <Col lg={4}>
+              <div className="banner_video">
+                <img src={video_pic} className="img-fluid plays" alt={video_pic}/>
+                <a href="/">
+                  <img src={play_point} width="70" alt={play_point}/>
+                </a>
+              </div>
+            </Col>
 
             <Col lg={12}>
                 
