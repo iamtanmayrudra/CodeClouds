81d448ed2c950b013d61a9c18b49bc5a2488f97a
tanmayrudra <tanmayrudra555@gmail.com>
2022-12-08 02:19:24 +0530

    Carousel Content Implements

--- src/commonComponents/Testimony.js
+++ src/commonComponents/Testimony.js
@@ -3,12 +3,16 @@ import React from 'react'
 // Bootstrap Module
 import { Container, Row, Col } from "react-bootstrap";
 
+import ReviewCarousel from "../commonComponents/ReviewCarousel"
+
 // Css
 import "./commonStyle/Testimony.scss";
 
 // Images import  
 import play_point from "../assets/ppc-play.svg";
-import video_pic from "../assets/video-pic.jpg";
+import jordan_pic from "../assets/clients/v-jordan-macdonald.jpg";
+import tim_pic from "../assets/clients/v-tim-hill.jpg";
+import dan_pic from "../assets/clients/v-dan-foster.jpg";
 
 
   function Testimony(props) {
@@ -23,7 +27,7 @@ import video_pic from "../assets/video-pic.jpg";
             </Col>
             <Col lg={4}>
               <div className="banner_video">
-                <img src={video_pic} className="img-fluid plays" alt={video_pic}/>
+                <img src={jordan_pic} className="img-fluid plays" alt="Jordan"/>
                 <a href="/">
                   <img src={play_point} width="70" alt={play_point}/>
                 </a>
@@ -31,7 +35,7 @@ import video_pic from "../assets/video-pic.jpg";
             </Col>
             <Col lg={4}>
               <div className="banner_video">
-                <img src={video_pic} className="img-fluid plays" alt={video_pic}/>
+                <img src={tim_pic} className="img-fluid plays" alt="Tim Hill"/>
                 <a href="/">
                   <img src={play_point} width="70" alt={play_point}/>
                 </a>
@@ -39,7 +43,7 @@ import video_pic from "../assets/video-pic.jpg";
             </Col>
             <Col lg={4}>
               <div className="banner_video">
-                <img src={video_pic} className="img-fluid plays" alt={video_pic}/>
+                <img src={dan_pic} className="img-fluid plays" alt="Dan Foster"/>
                 <a href="/">
                   <img src={play_point} width="70" alt={play_point}/>
                 </a>
@@ -47,7 +51,7 @@ import video_pic from "../assets/video-pic.jpg";
             </Col>
 
             <Col lg={12}>
-                
+                <ReviewCarousel />
             </Col>
           </Row>
         </Container>
