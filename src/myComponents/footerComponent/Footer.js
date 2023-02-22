dd253ee064e091f4058864e41effb56b65a91a95
tanmayrudra28091992 <69500423+tanmayrudra28091992@users.noreply.github.com>
2023-02-22 21:38:18 +0530

    Footer Buildding

--- src/myComponents/footerComponent/Footer.js
+++ src/myComponents/footerComponent/Footer.js
@@ -1,11 +1,46 @@
 import React from 'react'
 
+// Bootstrap Module
+import { Container, Row, Col } from "react-bootstrap";
+
+// Css
+import '../footerComponent/Footer.scss';
+
+// Material Icon
+// import MaterialIcon from 'react-google-material-icons';
+
+// Images import  
+import FooterLogo from "../../assets/footerElements/codeclouds_colour.svg";
+
+// React Router Dom
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
-    <>
-     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis veritatis dolorem provident. Numquam laboriosam labore totam sunt veritatis alias, molestiae, deleniti soluta delectus hic nesciunt repellendus dolorum, accusamus ducimus sapiente magnam distinctio at a. Ducimus id minus iure blanditiis. Reprehenderit distinctio odit, itaque eaque fugit inventore quo officia, vero maxime soluta nostrum amet facilis recusandae. Alias asperiores eveniet officiis maiores pariatur perferendis debitis voluptatum nisi itaque quisquam possimus, quod consequatur a nihil quam ipsam rerum. Voluptate aut harum distinctio cumque inventore iure similique voluptatibus laborum ipsam necessitatibus animi, porro et eligendi, ab voluptatum alias ducimus? Assumenda, ipsam in ipsum odit deserunt iusto quo laborum ducimus ab architecto aliquid accusantium nisi officia expedita eaque eveniet delectus sint vitae numquam sequi animi molestiae magni quidem distinctio. Nam eum minima reiciendis, labore vel ullam quidem ipsam veritatis nihil sed ducimus saepe omnis blanditiis similique assumenda quaerat laboriosam fugiat perspiciatis! Omnis id laborum cum sint accusamus natus! Illo, consequatur doloribus fuga laboriosam voluptatum ipsum soluta deserunt amet nihil unde, veniam consequuntur officiis. Veritatis possimus culpa architecto facilis sed molestiae minima repellendus obcaecati, expedita laboriosam!
-    </>
-  )
+    <div className="globalFooter">
+      <Container>
+        <Row>
+          <Col lg={3}>
+            <div className="footerAbout">
+              <Link to={"/"}>
+                <img src={FooterLogo} alt="Footer Logo" style={{width: "200px"}} />
+              </Link>
+            </div>
+          </Col>
+          <Col lg={9}>
+            <div className="footerNavs">
+              <Row>
+                <Col> qwdfgbwdfbvfwqwdfdq</Col>
+                <Col>ddwdv</Col>
+                <Col>sdvdwsdcv</Col>
+                <Col>dd</Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Footer
