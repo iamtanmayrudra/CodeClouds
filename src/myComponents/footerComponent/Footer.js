fbb18b31929b1574656457d855c115a7b9f82de8
tanmayrudra28091992 <69500423+tanmayrudra28091992@users.noreply.github.com>
2023-02-22 12:39:14 +0530

    Footer Updater

--- src/myComponents/footerComponent/Footer.js
+++ src/myComponents/footerComponent/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
-     This is Footer 
+     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis veritatis dolorem provident. Numquam laboriosam labore totam sunt veritatis alias, molestiae, deleniti soluta delectus hic nesciunt repellendus dolorum, accusamus ducimus sapiente magnam distinctio at a. Ducimus id minus iure blanditiis. Reprehenderit distinctio odit, itaque eaque fugit inventore quo officia, vero maxime soluta nostrum amet facilis recusandae. Alias asperiores eveniet officiis maiores pariatur perferendis debitis voluptatum nisi itaque quisquam possimus, quod consequatur a nihil quam ipsam rerum. Voluptate aut harum distinctio cumque inventore iure similique voluptatibus laborum ipsam necessitatibus animi, porro et eligendi, ab voluptatum alias ducimus? Assumenda, ipsam in ipsum odit deserunt iusto quo laborum ducimus ab architecto aliquid accusantium nisi officia expedita eaque eveniet delectus sint vitae numquam sequi animi molestiae magni quidem distinctio. Nam eum minima reiciendis, labore vel ullam quidem ipsam veritatis nihil sed ducimus saepe omnis blanditiis similique assumenda quaerat laboriosam fugiat perspiciatis! Omnis id laborum cum sint accusamus natus! Illo, consequatur doloribus fuga laboriosam voluptatum ipsum soluta deserunt amet nihil unde, veniam consequuntur officiis. Veritatis possimus culpa architecto facilis sed molestiae minima repellendus obcaecati, expedita laboriosam!
     </>
   )
 }
